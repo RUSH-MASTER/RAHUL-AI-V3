b77d062829661c327a5ae04c2c460c2abe88af05
RAHUL-MASTER <rahulhiran37@gmail.com>
2025-04-01 11:16:25 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -48,7 +48,7 @@ Hey 👋🏻 ${pushname}
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363318387454868@newsletter',
+                    newsletterJid: '120363402970588262@newsletter',
                     newsletterName: 'RAHUL-AI-V3 ⭐',
                     serverMessageId: 143
                 }
